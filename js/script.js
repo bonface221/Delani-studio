2640847e9ec441e485a0ebd211aa8485ec956f19
[bonface221] <[mainaboniface221@gmail.com]>
2022-03-13 16:11:57 +0300

    file:javascript-added the js file

---
+++ js/script.js
@@ -0,0 +1,41 @@
+$(document).ready(function () {
+    
+    $("#whatDesign").click(function () {
+        $(".pDesign").toggle();
+        $("#whatDesign").toggle();
+       
+    });
+    $(".pDesign").click(function () {
+        $("#whatDesign").toggle();
+        $(".pDesign").toggle()
+    });
+
+    $("#whatDevelopment").click(function () {
+		$(".pDevelopment").toggle();
+		$("#whatDevelopment").toggle();
+	});
+	$(".pDevelopment").click(function () {
+		$("#whatDevelopment").toggle();
+		$(".pDevelopment").toggle();
+    });
+    
+    $("#whatManagement").click(function () {
+		$(".pManagement").toggle();
+		$("#whatManagement").toggle();
+	});
+	$(".pManagement").click(function () {
+		$("#whatManagement").toggle();
+		$(".pManagement").toggle();
+	});
+    
+
+
+
+
+
+
+
+
+
+
+})
